73b075aa4690792f0a8b276170f7d3b9f307abf6
AKKATtone <plutoboy97@gmail.com>
2019-08-02 12:23:57 +0200

    (quasi) completamento semPost()
    fix di alcuni problemi e stesura del resto del corpo principale della funzione

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -21,4 +21,34 @@ void internal_semPost(){
     
     Semaphore* sem = sem_desc->semaphore; //prendo il semaforo associato al descrittore
     sem->count++; //incremento contatore di semafori
+    if(sem->count <= 0){
+		//estraggo il primo descrittore del semaforo dalla lista dei descrittori in attesa
+		SemDescriptorPtr* d_ptr_aux = (SemDescriptorPtr*) List_detach(&sem->waiting_descriptors, (ListItem*) sem->waiting_descriptors.first); 
+		if(!ptr_aux){
+			printf("[SEMAFORI] errore nella rimozione del descrittore dalla lista di waiting del semaforo\n");
+			running->syscall_retvalue = DSOS_ELIST_DETACH;
+			return;
+		}
+		//inserisco il puntatore al descrittore nella lista dei descrittori del semaforo
+		SemDescriptorPtr* dptr = (SemDescriptorPtr*) List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) d_ptr_aux);
+		if(!dprt){
+			printf("[SEMAFORI] errore nell'inserimetno del puntatore a descrittore nella lista dei descrittori del semaforo\n");
+			running->syscall_retvalue= DSOS_ELIST_INSERT;
+		}
+		//imposto lo stato del pcb del processo attuale in ready
+		PCB* exPCB = d_ptr_aux->descriptor->pcb;
+		exPCB->status = Ready;
+		//sposto il pcb del processo corrente nella lista di ready del sistema operativo
+		PCB* pcbAux = (PCB*)List_detach(&waiting_list, (ListItem*) exPCB);
+		if(pcbAux==NULL){
+				printf("[SEMAFORI] errore nella rimozione del processo dalla waiting list\n");
+				running->syscall_retvalue= DSOS_ELIST_DETACH;
+		}
+		//inserisco il processo attuale nella fila di ready
+		pcbAux = (PCB*)List_insert(&ready_list, ready_list.last, (ListItem*) exPCB);
+        if(!pcbAux){
+			printf("[SEMAFORO] errore nell'inserimento del processo nella fila di ready\n")
+			running->syscall_retvalue = DSOS_ELIST_INSERT;
+		}
+	}
 }
