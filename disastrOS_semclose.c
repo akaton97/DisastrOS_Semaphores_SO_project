83fe9c3be4950d1348735c0cb4058520c6a6ec7e
Davide <davimass97@gmail.com>
2019-08-10 12:06:33 +0200

    Modifica della semClose
    fix generali del codice in particolare della free

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -9,7 +9,7 @@
 void internal_semClose(){
 	
 	int semid = running -> syscall_args[0];
-	void* ret;
+	int ret ;
 	
 	//ricerca descrittore
 	SemDescriptor* sds = SemDescriptorList_byFd(&running->sem_descriptors, semid);
@@ -20,18 +20,50 @@ void internal_semClose(){
 	}
 	
 	//rimozione descrittore
-	ret = (SemDescriptor*)List_detach(&running->sem_descriptors, (ListItem*)sds);
-	if(ret== NULL){
+	sds = (SemDescriptor*)List_detach(&running->sem_descriptors, (ListItem*)sds);
+	if(sds== NULL){
 		printf("errore nella rimozione del descrittore");
 		return;
 	}
 	
+	//semaforo associato al descrittore
 	Semaphore* sem = sds->semaphore;
 	
-	//rimozione puntatore
-	ret = List_detach(&sem->descriptors, (ListItem*)sds->ptr);
-	if(ret==NULL){
-		printf("errore nella rimozione del puntatore");
+	//prendo il puntatore dalla lista semafori per chiamare poi la free
+	SemDescriptorPtr* sdptr = (SemDescriptor*)List_detach(&sem->descriptors, (ListItem*)sds->ptr);
+	if(!sdptr){
+		printf("errore nella rimozione del ptr al descrittore dalla lista");
 		return;
 	}
+	
+	ret = SemDescriptorPtr_free(sdptr);
+	if(ret){
+		printf("errore nella free del ptr al descrittore");
+		return;
+	}
+	
+	ret = SemDescriptor_free(sds);
+	if(ret){
+		printf("errore nella free del descrittore");
+		return;
+	}
+	
+	//faccio una verifica e chiudo il semaforo 
+	if(sem->descriptors.size == 0 && sem->waiting_descriptors.size == 0){
+		printf("chiusura semaforo");
+		Semaphore* aux = (Semaphore*)List_detach(&semaphores_list,(ListItem*)sem);
+		if(!aux){
+			printf("errore rimozione semaforo");
+			return;
+		}
+		ret = Semaphore_free(sem);
+		if(ret){
+			printf("errore free del semaforo");
+			return;
+		}
+		disastrOS_printStatus();
+	}
+		
+	running->last_sem_fd --;
+	
 }
